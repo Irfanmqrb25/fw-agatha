c7716a4be8838b1d860907b09ef1bc64ed36bb5d
lingkunganAgatha <agathapasarbaru@gmail.com>
2025-07-27 13:18:37 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,9 @@ import { redirect } from "next/navigation"
 export default async function Home() {
   const session = await auth()
 
-  // Jika sudah login, arahkan ke dashboard
   if (session?.user?.role) {
     redirect("/dashboard")
   }
 
-  // Jika belum login, arahkan ke login
   redirect("/login")
 }
